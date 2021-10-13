48d0461bed1623b3b20bbed33c3eb4d4736ee8fe
Joyce <1983276@cegepmontpetit.ca>
2021-10-13 15:06:00 -0400

    Creation de la liste de questions

--- VoteDroid/app/src/main/java/com/example/votedroid/MainActivity.java
+++ VoteDroid/app/src/main/java/com/example/votedroid/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 
@@ -11,9 +13,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.votedroid.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,13 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+    /*    listView=(ListView)findViewById(R.id.listquestion);
+        ArrayList<String> Questions = new ArrayList<>();
+        Questions.add("joyce");
+        Questions.add("Fait il beau aujourdhui");
+
+        ArrayAdapter QuestionAdapter = new ArrayAdapter(this,android.res.layout.simple_List_item_1,Questions); */
 
         binding.buttonAjouter.setOnClickListener(new View.OnClickListener() {
             @Override
