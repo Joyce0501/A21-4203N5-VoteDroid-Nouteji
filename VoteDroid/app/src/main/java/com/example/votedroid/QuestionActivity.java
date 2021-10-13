c53bebc7c78f789accc4b6f3b869b61892c609a5
Joyce <1983276@cegepmontpetit.ca>
2021-10-13 13:13:46 -0400

    Lien entre la page question et accueil
    retour en cliquant sur le bouton poser la question

--- VoteDroid/app/src/main/java/com/example/votedroid/QuestionActivity.java
+++ VoteDroid/app/src/main/java/com/example/votedroid/QuestionActivity.java
@@ -1,5 +1,6 @@
 package com.example.votedroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -21,5 +22,13 @@ public class QuestionActivity extends AppCompatActivity {
         binding = ActivityQuestionBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        binding.buttonPoser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent accueil = new Intent(QuestionActivity.this,MainActivity.class);
+                startActivity(accueil);
+            }
+        });
     }
 }
